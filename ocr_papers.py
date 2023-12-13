90d79f0a685ba41fea9402e068ec734b03ffef65
Zhi Yong Tan <yongish@gmail.com>
2023-12-12 20:39:26 -0800

    debug

--- ocr_papers.py
+++ ocr_papers.py
@@ -4,11 +4,10 @@ import datetime
 import easyocr
 import io
 import os
+import PyPDF2
+
 
 from joblib import Parallel, delayed
-# def process(i):
-#     return i * i
-# results = Parallel(n_jobs=2)(delayed(process)(i) for i in range(10))
 
 reader = easyocr.Reader(['ch_sim','en'])
 def getWordList(image):
@@ -19,27 +18,42 @@ def getWordList(image):
   return words
 
 
+input_dir = './pdfs'
 # for i, file in enumerate(os.listdir(input_dir)):
 def process(i, file):
-  print(f'{datetime.datetime.now()}. file: {file}. index:{i}')
+  filename_no_extension, file_extension = os.path.splitext(file)
+  if file_extension != '.pdf':
+    return
 
-  # todo: Skip files that have already been seen.
-  filename = os.fsdecode(file)
+print(f'index: {i} STARTED at {datetime.datetime.now()}. file: {file}')
+
+  filepath = os.path.join(input_dir, file)
+  pdffile = open(filepath, 'rb')
+  pdfReader = PyPDF2.PdfReader(pdffile)
+  totalPages = len(pdfReader.pages)
 
   #
   # filename = '2015-P6-Prelims-Chinese-CHIJ.pdf'
 
-  filename_no_extension = Path(filename).stem
   output_folder = os.path.join('raw', filename_no_extension)
+  #if os.exists(output_folder):
+  # return
   Path(output_folder).mkdir(parents=True, exist_ok=True)
 
-  images = convert_from_path(os.path.join(input_dir, filename), dpi=500)
+  numFiles = len([name for name in os.listdir(output_folder) if os.path.isfile(os.path.join(output_folder, name))])
+  print(f'No. of files in {output_folder} :{numFiles}')
+  print(f'Total pages in {filepath} :{totalPages}')
+
+  if numFiles == totalPages - 1:
+    return
+
+  images = convert_from_path(filepath, dpi=300)
   for page_number, image in enumerate(images[1:]):
     words = getWordList(image)
     with open(os.path.join(output_folder, f'page{page_number + 1}.txt'), 'w') as f:
       for word in words:
         f.write(f'{word}\n')
+  
+  print(f'index: {i} ENDED at {datetime.datetime.now()}. file: {file}')
 
-input_dir = './pdfs'
 Parallel(n_jobs=8)(delayed(process)(i, file) for i, file in enumerate(os.listdir(input_dir)))
-
